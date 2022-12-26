0125edbdcf2b953d1160b92cf10b485e8955031a
Saiyan Abhishek <74703491+geeky-abhishek@users.noreply.github.com>
2022-12-26 18:36:48 +0530

    test

--- src/component/Home/ApplicationStatus.tsx
+++ src/component/Home/ApplicationStatus.tsx
@@ -33,6 +33,7 @@ const ApplicationStatus: FC<PropType> = () => {
     [downloadList]
   );
 
+  
   return (
     <div style={{ padding: "50px" }}>
       <Table className="font-regular bg-white bg-opacity-70 rounded mb-16">
